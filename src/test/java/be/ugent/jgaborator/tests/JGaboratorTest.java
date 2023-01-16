8fc30185ac1bdac8ffbfaba0d27a9877781408e2
Joren Six <joren@0110.be>
2023-01-16 12:13:28 +0100

    CI

--- src/test/java/be/ugent/jgaborator/tests/JGaboratorTest.java
+++ src/test/java/be/ugent/jgaborator/tests/JGaboratorTest.java
@@ -1,10 +1,13 @@
 package be.ugent.jgaborator.tests;
 
 import be.tarsos.dsp.AudioDispatcher;
+import be.tarsos.dsp.io.TarsosDSPAudioFloatConverter;
 import be.tarsos.dsp.io.jvm.AudioDispatcherFactory;
+import be.tarsos.dsp.io.jvm.JVMAudioInputStream;
 import be.ugent.jgaborator.JGaborator;
 import org.junit.jupiter.api.Test;
 
+import javax.sound.sampled.AudioFormat;
 import javax.sound.sampled.AudioInputStream;
 import javax.sound.sampled.AudioSystem;
 import javax.sound.sampled.UnsupportedAudioFileException;
@@ -18,10 +21,10 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class JGaboratorTest {
     @Test
     public void runOnFile(){
-        final URL url = ClassLoader.getSystemResource("44.1kHz_440Hz_1s.wav");
-        System.out.println(url);
+        final float[]  audioSamples = audioBufferFile("44.1kHz_440Hz_1s.wav",44100);
+
         try {
-            AudioDispatcher adp = AudioDispatcherFactory.fromURL(url,1024,0);
+            AudioDispatcher adp = AudioDispatcherFactory.fromFloatArray(audioSamples,44100,1024,0);
 
             final JGaborator zsazsa = new JGaborator(1024 , 44100, 24, 220,880,440.5  ,512);
 
@@ -44,9 +47,25 @@ public class JGaboratorTest {
             assertEquals(440,zsazsa.bandCenters(maxIndex),0.501);
         } catch (UnsupportedAudioFileException e) {
             throw new RuntimeException(e);
-        } catch (IOException e) {
-            throw new RuntimeException(e);
         }
 
+    }
+
+    private static float[] audioBufferFile(String file,int lengthInSamples){
+        float[] buffer = new float[lengthInSamples];
+        try {
+            final URL url = ClassLoader.getSystemResource(file);
+            AudioInputStream audioStream = AudioSystem.getAudioInputStream(url);
+            AudioFormat format = audioStream.getFormat();
+            TarsosDSPAudioFloatConverter converter = TarsosDSPAudioFloatConverter.getConverter(JVMAudioInputStream.toTarsosDSPFormat(format));
+            byte[] bytes = new byte[lengthInSamples * format.getSampleSizeInBits()];
+            audioStream.read(bytes);
+            converter.toFloatArray(bytes, buffer);
+        } catch (IOException e) {
+            throw new Error("Test audio file should be present.");
+        } catch (UnsupportedAudioFileException e) {
+            throw new Error("Test audio file format should be supported.");
+        }
+        return buffer;
     }
 }
